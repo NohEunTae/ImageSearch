a8ab07d4569c0bc9f879725a2886e974a50d9d45
NohEunTae <dmsxoeldk@naver.com>
2019-08-28 21:24:44 +0900

    Networking #1
    Alamorfire를 이용해서 검색한 단어의 URL에 대한 Request 요청
    
    response의 결과에 따라 Observable을 설정하고 Observable을 반환

---
+++ ImageSearch/ImageSearch/Modules/Networking.swift
@@ -0,0 +1,48 @@
+//
+//  Networking.swift
+//  ImageSearch
+//
+//  Created by user on 26/08/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+struct Networking {
+    static private let baseUrl = "https://dapi.kakao.com/v2/search/image?query="
+    static private let key = ["Authorization": "KakaoAK 57c7624a2478f7c7ebc1678f82867b69"]
+    
+    static func request<T: Codable>(param: String) -> Observable<T> {
+        var str_search = baseUrl + param
+        
+        return Observable<T>.create({ observer in
+            str_search = str_search.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+            let dataRequest = Alamofire.request(str_search,
+                                                method: .get,
+                                                encoding: JSONEncoding.prettyPrinted,
+                                                headers: key)
+                .responseJSON{ response in
+                    switch response.result {
+                    case .success(let json):
+                        do {
+                            let jsonData = try JSONSerialization.data(withJSONObject: json)
+                            let decoder = JSONDecoder()
+                            decoder.keyDecodingStrategy = .convertFromSnakeCase
+                            let model : T = try decoder.decode(T.self, from: jsonData)
+                            observer.onNext(model)
+                        } catch let error {
+                            observer.onError(error)
+                        }
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                    observer.onCompleted()
+            }
+            return Disposables.create {
+                dataRequest.cancel()
+            }
+        })
+    }
+}
